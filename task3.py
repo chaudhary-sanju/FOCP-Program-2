ee535cccc4e8f60b604ec8faa45092175092af12
Sanju <Sashin.chaudhary5@gmail.com>
2022-10-19 12:09:57 +0545

    code modified into function

--- task3.py
+++ task3.py
@@ -1,5 +1,8 @@
-total_std = int(input("How many students? "))
-size = int(input("Required group size? "))
-group = int(total_std / size)
-left_over = total_std % size
-print(f"There will be {group} groups with {left_over} student left over")
+def group_division():
+    total_std = int(input("How many students? "))
+    size = int(input("Required group size? "))
+    group = int(total_std / size)
+    left_over = total_std % size
+    return(f"There will be {group} groups with {left_over} student left over")
+
+print(group_division())
--- task4.py
+++ task4.py
@@ -1,6 +1,8 @@
-total_sweets = int(input("How many sweets? "))
-stds = int(input("Number of Student Present in Class? "))
-each_std_gets = int(total_sweets / stds)
-left_over = total_sweets % stds
-print(f"Each Student will get {each_std_gets} sweets.")
-print(f"left over sweets are {left_over}.")
+def sweet_division():
+    total_sweets = int(input("How many sweets? "))
+    stds = int(input("Number of Student Present in Class? "))
+    each_std_gets = int(total_sweets / stds)
+    left_over = total_sweets % stds
+    return(f"Each Student will get {each_std_gets} sweets. \nleft over sweets are {left_over}.")
+    
+print(sweet_division())
